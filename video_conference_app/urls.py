55d79115e1e3b5589734c152c2b67015145e4b83
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 01:05:10 +0000

    feat: Add URL for User Registration

--- video_conference_app/urls.py
+++ video_conference_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('register/',views.register, name='register'),
+    path('register/',views.register, name='register'),
     # path('login/',views.login_view, name='login'),
     # path('dashboard/',views.dashboard, name='dashboard'),
     # path('meeting/',views.videocall, name='meeting'),
