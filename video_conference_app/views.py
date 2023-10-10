8da1990ee9f9e8cb62794ebaaf9be2e07a12827d
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 00:55:23 +0000

    Initial Commit

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -1,38 +1,38 @@
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render, redirect
+# from .forms import RegisterForm
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
 
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'login.html', {'success': "Registration successful. Please login."})
-        else:
-            error_message = form.errors.as_text()
-            return render(request, 'register.html', {'error': error_message})
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return render(request, 'login.html', {'success': "Registration successful. Please login."})
+#         else:
+#             error_message = form.errors.as_text()
+#             return render(request, 'register.html', {'error': error_message})
 
-    return render(request, 'register.html')
+#     return render(request, 'register.html')
 
 
-def login_view(request):
-    if request.method=="POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("/dashboard")
-        else:
-            return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
+# def login_view(request):
+#     if request.method=="POST":
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=email, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect("/dashboard")
+#         else:
+#             return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
 
-    return render(request, 'login.html')
+#     return render(request, 'login.html')
 
 # @login_required
 # def dashboard(request):
@@ -42,10 +42,10 @@ def login_view(request):
 # def videocall(request):
 #     return render(request, 'videocall.html', {'name': request.user.first_name + " " + request.user.last_name})
 
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect("/login")
+# @login_required
+# def logout_view(request):
+#     logout(request)
+#     return redirect("/login")
 
 # @login_required
 # def join_room(request):
