dae0287d88d7b65f2271a94ef936aabcb1c1a380
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 00:58:33 +0000

    feat: Add view to handle User Login

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -21,18 +21,18 @@ def register(request):
     return render(request, 'register.html')
 
 
-# def login_view(request):
-#     if request.method=="POST":
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=email, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect("/dashboard")
-#         else:
-#             return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
+def login_view(request):
+    if request.method=="POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/dashboard")
+        else:
+            return render(request, 'login.html', {'error': "Invalid credentials. Please try again."})
 
-#     return render(request, 'login.html')
+    return render(request, 'login.html')
 
 # @login_required
 # def dashboard(request):
