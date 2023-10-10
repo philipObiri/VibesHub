22ceb7cfd9cf0c907667742a29b2c6b3a339124a
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 01:02:19 +0000

    feat: View to allow users join a spcific room

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -47,9 +47,9 @@ def logout_view(request):
     logout(request)
     return redirect("/login")
 
-# @login_required
-# def join_room(request):
-#     if request.method == 'POST':
-#         roomID = request.POST['roomID']
-#         return redirect("/meeting?roomID=" + roomID)
-#     return render(request, 'joinroom.html')
+@login_required
+def join_room(request):
+    if request.method == 'POST':
+        roomID = request.POST['roomID']
+        return redirect("/meeting?roomID=" + roomID)
+    return render(request, 'joinroom.html')
