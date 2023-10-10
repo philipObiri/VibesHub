004cbdc92d61f8cbf990b645d4d28c4612542f95
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 14:43:50 +0000

    Initial commit

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
-# # Create your views here.
 
+# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
