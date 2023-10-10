e9a6410e73ba47c80e824c74b102a978c239a96a
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 00:57:48 +0000

    feat: Add a view to handle user registration and validation:

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -8,17 +8,17 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html')
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = RegisterForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return render(request, 'login.html', {'success': "Registration successful. Please login."})
-#         else:
-#             error_message = form.errors.as_text()
-#             return render(request, 'register.html', {'error': error_message})
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html', {'success': "Registration successful. Please login."})
+        else:
+            error_message = form.errors.as_text()
+            return render(request, 'register.html', {'error': error_message})
 
-#     return render(request, 'register.html')
+    return render(request, 'register.html')
 
 
 # def login_view(request):
