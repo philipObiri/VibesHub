93dbb0dcf9b8688af3ee9ef52f39bc091115bf34
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 01:05:49 +0000

    feat: Add URL for User Login

--- video_conference_app/urls.py
+++ video_conference_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/',views.register, name='register'),
-    # path('login/',views.login_view, name='login'),
+    path('login/',views.login_view, name='login'),
     # path('dashboard/',views.dashboard, name='dashboard'),
     # path('meeting/',views.videocall, name='meeting'),
     # path('logout/',views.logout_view, name='logout'),
