c23b1cd94cfe650cf46fc8ff0fbd0865b534028e
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 00:56:45 +0000

    feat: Add view to render Homepage

--- video_conference_app/views.py
+++ video_conference_app/views.py
@@ -1,12 +1,12 @@
-# from django.shortcuts import render, redirect
-# from .forms import RegisterForm
-# from django.contrib.auth import authenticate, login, logout
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # # Create your views here.
 
-# def index(request):
-#     return render(request, 'index.html')
+def index(request):
+    return render(request, 'index.html')
 
 # def register(request):
 #     if request.method == 'POST':
