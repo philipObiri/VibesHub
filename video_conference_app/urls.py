74b9a8bdf6007f12709356b57e9ca99be7f9682c
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-10 01:07:49 +0000

    feat : Add URL for User Logout

--- video_conference_app/urls.py
+++ video_conference_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/',views.login_view, name='login'),
     path('dashboard/',views.dashboard, name='dashboard'),
     path('meeting/',views.videocall, name='meeting'),
-    # path('logout/',views.logout_view, name='logout'),
+    path('logout/',views.logout_view, name='logout'),
     # path('join/',views.join_room, name='join_room'),
     path('',views.index, name='index'),
 
